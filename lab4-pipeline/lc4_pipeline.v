7c316e1871da07124f59433645b9fb481448fc70
Yang <1575863102@qq.com>
2024-07-02 10:00:40 -0400

    V1.1.0

--- lab4-pipeline/lc4_pipeline.v
+++ lab4-pipeline/lc4_pipeline.v
@@ -1,5 +1,3 @@
-/* TODO: name and PennKeys of all group members here */
-
 `timescale 1ns / 1ps
 
 // disable implicit wire declaration
@@ -34,36 +32,239 @@ module lc4_processor
     );
    
    /*** YOUR CODE HERE ***/
-   assign test_stall = 2'b10; 
+   assign test_stall = ; 
    // pc wires attached to the PC register's ports
    wire [15:0]   pc;      // Current program counter (read out from pc_reg)
    wire [15:0]   next_pc; // Next program counter (you compute this and feed it into next_pc)
 
-   // Program counter register, starts at 8200h at bootup
+   // 定义流水线寄存器信号
+   wire [15:0] i_if_id_insn, o_if_id_insn;
+   wire [15:0] i_if_id_pc, o_if_id_pc;
+   wire [15:0] i_id_ex_insn, o_id_ex_insn;
+   wire [15:0] i_id_ex_pc, o_id_ex_pc;
+   wire [15:0] i_id_ex_alu_src1, o_id_ex_alu_src1;
+   wire [15:0] i_id_ex_alu_src2, o_id_ex_alu_src2;
+   wire [15:0] i_ex_mem_insn, o_ex_mem_insn;
+   wire [15:0] i_ex_mem_alu_result, o_ex_mem_alu_result;
+   wire [15:0] i_ex_mem_dmem_towrite, o_ex_mem_dmem_towrite;
+   wire [15:0] i_mem_wb_insn, o_mem_wb_insn;
+   wire [15:0] i_mem_wb_alu_result, o_mem_wb_alu_result;
+   wire [15:0] i_mem_wb_dmem_data, o_mem_wb_dmem_data;
+
+   wire if_id_we, id_ex_we, ex_mem_we, mem_wb_we;
+
+   // 解码信号的流水线寄存器
+   wire [8:0] i_id_ex_sel, o_id_ex_sel;
+   wire [8:0] i_id_ex_decode, o_id_ex_decode;
+   wire [8:0] i_ex_mem_sel, o_ex_mem_sel;
+   wire [8:0] i_ex_mem_decode, o_ex_mem_decode;
+   wire [8:0] i_mem_wb_sel, o_mem_wb_sel;
+   wire [8:0] i_mem_wb_decode, o_mem_wb_decode;
+   // Program Counter (pc_reg)
    Nbit_reg #(16, 16'h8200) pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
-   //流水线寄存器
-   //Fetch
-   wire [15:0]   i_F_date;  
-   wire [15:0]   o_F_date; 
-   Nbit_reg #(16, 16'h0000) F_reg (.in(i_F_date), .out(o_F_date), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   //Decode
-   wire [15:0]   i_D_date;  
-   wire [15:0]   o_D_date; 
-   Nbit_reg #(16, 16'h0000) D_reg (.in(i_D_date), .out(o_D_date), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   //Xecute
-   wire [15:0]   i_X_date;  
-   wire [15:0]   o_X_date; 
-   Nbit_reg #(16, 16'h0000) X_reg (.in(i_X_date), .out(o_X_date), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   //Memory
-   wire [15:0]   i_M_date;  
-   wire [15:0]   o_M_date; 
-   Nbit_reg #(16, 16'h0000) M_reg (.in(i_M_date), .out(o_M_date), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));   
-   //Writeback
-   wire [15:0]   i_M_date;  
-   wire [15:0]   o_M_date; 
-   Nbit_reg #(16, 16'h0000) M_reg (.in(i_M_date), .out(o_M_date), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));    
+   // IF-ID Pipeline Register
+   Nbit_reg #(16, 16'h0000) if_id_insn_reg (.in(i_if_id_insn), .out(o_if_id_insn), .clk(clk), .we(if_id_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) if_id_pc_reg (.in(i_if_id_pc), .out(o_if_id_pc), .clk(clk), .we(if_id_we), .gwe(gwe), .rst(rst));
 
+   // ID-EX Pipeline Register
+   Nbit_reg #(16, 16'h0000) id_ex_insn_reg (.in(i_id_ex_insn), .out(o_id_ex_insn), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) id_ex_pc_reg (.in(i_id_ex_pc), .out(o_id_ex_pc), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) id_ex_alu_src1_reg (.in(i_id_ex_alu_src1), .out(o_id_ex_alu_src1), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) id_ex_alu_src2_reg (.in(i_id_ex_alu_src2), .out(o_id_ex_alu_src2), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+   
+   Nbit_reg #(9, 9'b000000000) id_ex_sel_reg (.in(i_id_ex_sel), .out(o_id_ex_sel), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(9, 9'b000000000) id_ex_decode_reg (.in(i_id_ex_decode), .out(o_id_ex_decode), .clk(clk), .we(id_ex_we), .gwe(gwe), .rst(rst));
+
+   // EX-MEM Pipeline Register
+   Nbit_reg #(16, 16'h0000) ex_mem_insn_reg (.in(i_ex_mem_insn), .out(o_ex_mem_insn), .clk(clk), .we(ex_mem_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) ex_mem_alu_result_reg (.in(i_ex_mem_alu_result), .out(o_ex_mem_alu_result), .clk(clk), .we(ex_mem_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) ex_mem_dmem_towrite_reg (.in(i_ex_mem_dmem_towrite), .out(o_ex_mem_dmem_towrite), .clk(clk), .we(ex_mem_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(9, 9'b000000000) ex_mem_sel_reg (.in(i_ex_mem_sel), .out(o_ex_mem_sel), .clk(clk), .we(ex_mem_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(9, 9'b000000000) ex_mem_decode_reg (.in(i_ex_mem_decode), .out(o_ex_mem_decode), .clk(clk), .we(ex_mem_we), .gwe(gwe), .rst(rst));
+
+   // MEM-WB Pipeline Register
+   Nbit_reg #(16, 16'h0000) mem_wb_insn_reg (.in(i_mem_wb_insn), .out(o_mem_wb_insn), .clk(clk), .we(mem_wb_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) mem_wb_alu_result_reg (.in(i_mem_wb_alu_result), .out(o_mem_wb_alu_result), .clk(clk), .we(mem_wb_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) mem_wb_dmem_data_reg (.in(i_mem_wb_dmem_data), .out(o_mem_wb_dmem_data), .clk(clk), .we(mem_wb_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(9, 9'b000000000) mem_wb_sel_reg (.in(i_mem_wb_sel), .out(o_mem_wb_sel), .clk(clk), .we(mem_wb_we), .gwe(gwe), .rst(rst));
+   Nbit_reg #(9, 9'b000000000) mem_wb_decode_reg (.in(i_mem_wb_decode), .out(o_mem_wb_decode), .clk(clk), .we(mem_wb_we), .gwe(gwe), .rst(rst));
+
+
+   // Signals for the testbench
+   assign test_cur_pc = pc;
+   assign test_cur_insn = i_cur_insn;
+   assign test_regfile_we = regfile_we; 
+   assign test_regfile_wsel = wsel;
+   assign test_regfile_data = i_wdata;
+   assign test_nzp_we = nzp_we;    
+   assign test_nzp_new_bits = new_nzp_bits;
+   assign test_dmem_we = o_dmem_we;   
+   assign test_dmem_addr = o_dmem_addr;
+   assign test_dmem_data = o_dmem_towrite;   
+
+   // NZP寄存器定义
+   reg [2:0] nzp; // NZP寄存器
+
+   // 计算新的NZP值
+   wire [2:0] new_nzp_bits;
+   assign new_nzp_bits = (i_wdata[15] == 1'b1) ? 3'b100 : // N位
+                        (i_wdata == 16'b0) ? 3'b010 :   // Z位
+                        3'b001;                         // P位
+
+   // 在写入寄存器时更新NZP
+   always @(posedge clk) begin
+      if (rst) begin
+         nzp <= 3'b000;
+      end else if (o_mem_wb_decode[3] && gwe) begin
+         nzp <= new_nzp_bits;
+      end
+   end
+
+   wire [2:0] branch_condition = alu_insn[11:9];
+
+   // Determine if the branch should be taken
+   wire take_branch = (branch_condition == 3'b000) ? 1'b0 : // No branch
+                     (branch_condition == 3'b001) ? nzp[0] : // BRp: branch if positive
+                     (branch_condition == 3'b010) ? nzp[1] : // BRz: branch if zero
+                     (branch_condition == 3'b011) ? (nzp[0] | nzp[1]) : // BRzp: branch if zero or positive
+                     (branch_condition == 3'b100) ? nzp[2] : // BRn: branch if negative
+                     (branch_condition == 3'b101) ? (nzp[0] | nzp[2]) : // BRnp: branch if negative or positive
+                     (branch_condition == 3'b110) ? (nzp[1] | nzp[2]) : // BRnz: branch if negative or zero
+                     (branch_condition == 3'b111) ? (nzp[0] | nzp[1] | nzp[2]) : // BRnzp: branch if negative, zero, or positive
+                     1'b0; // Default case
+
+   // Fetch
+   wire [15:0] s_next_pc;
+
+   cla16 u_cla16(
+      .a    ( pc    ),
+      .b    ( 16'h0001  ),
+      .cin  ( 1'b0  ),
+      .sum  ( s_next_pc )
+   );
+
+   assign next_pc = (is_branch & take_branch | is_control_insn) ? alu_result : s_next_pc;
+   assign i_if_id_insn = i_cur_insn;
+   assign i_if_id_pc = pc;
+   assign if_id_we = (i_cur_insn != 16'b0);
+
+   // Decode阶段
+   wire [2:0]   r1sel;
+   wire         r1re;
+   wire [2:0]   r2sel;
+   wire         r2re;
+   wire [2:0]   wsel;
+   wire         regfile_we;
+   wire         nzp_we;
+   wire         select_pc_plus_one;
+   wire         is_load;
+   wire         is_store;
+   wire         is_branch;
+   wire         is_control_insn;
+
+   lc4_decoder u_lc4_decoder(
+      .insn                ( o_if_id_insn        ),
+      .r1sel               ( r1sel               ),
+      .r1re                ( r1re                ),
+      .r2sel               ( r2sel               ),
+      .r2re                ( r2re                ),
+      .wsel                ( wsel                ),
+      .regfile_we          ( regfile_we          ),
+      .nzp_we              ( nzp_we              ),
+      .select_pc_plus_one  ( select_pc_plus_one  ),
+      .is_load             ( is_load             ),
+      .is_store            ( is_store            ),
+      .is_branch           ( is_branch           ),
+      .is_control_insn     ( is_control_insn     )
+   );
+
+   wire [15:0] 	o_rs_data;
+   wire [15:0] 	o_rt_data;
+   wire [15:0]    i_wdata;
+
+   lc4_regfile #(
+      .n 	( 16  ))
+   u_lc4_regfile(
+      .clk       	( clk        ),
+      .gwe       	( gwe        ),
+      .rst       	( rst        ),
+      .i_rs      	( r1sel       ),
+      .o_rs_data 	( o_rs_data  ),
+      .i_rt      	( r2sel       ),
+      .o_rt_data 	( o_rt_data  ),
+      .i_rd      	( wsel       ),
+      .i_wdata   	( i_wdata    ),
+      .i_rd_we   	( regfile_we )
+   );
+
+
+
+   assign i_id_ex_insn = o_if_id_insn;
+   assign i_id_ex_pc = pc;
+   assign i_id_ex_alu_src1 = o_rs_data;
+   assign i_id_ex_alu_src2 = o_rt_data;
+
+   assign id_ex_we = 1'b1;
+
+   assign i_id_ex_sel[2:0] = r1sel;
+   assign i_id_ex_sel[5:3] = r2sel;
+   assign i_id_ex_sel[8:6] = wsel;
+   assign i_id_ex_decode[0] = r1re;
+   assign i_id_ex_decode[1] = r2re;
+   assign i_id_ex_decode[2] = regfile_we;
+   assign i_id_ex_decode[3] = nzp_we;
+   assign i_id_ex_decode[4] = select_pc_plus_one;
+   assign i_id_ex_decode[5] = is_load;
+   assign i_id_ex_decode[6] = is_store;
+   assign i_id_ex_decode[7] = is_branch;
+   assign i_id_ex_decode[8] = is_control_insn;
+
+   // Execute阶段
+   wire [15:0]   alu_insn; 
+   wire [15:0]   alu_pc; 
+   wire [15:0]   alu_r1data, alu_r2data;
+   wire [15:0]   alu_result;
+
+   lc4_alu u_lc4_alu(
+      .i_insn   ( alu_insn       ),
+      .i_pc     ( alu_pc         ),
+      .i_r1data ( alu_r1data     ),
+      .i_r2data ( alu_r2data     ),
+      .o_result ( alu_result     )
+   );
+
+   assign alu_insn   = o_id_ex_insn;
+   assign alu_pc     = o_id_ex_pc;
+   assign alu_r1data = o_id_ex_alu_src1;
+   assign alu_r2data = o_id_ex_alu_src2;
+
+   assign i_ex_mem_insn = o_id_ex_insn;
+   assign i_ex_mem_alu_result = alu_result;
+   assign i_ex_mem_dmem_towrite = o_id_ex_alu_src2;
+   assign ex_mem_we = 1'b1;
+   assign i_ex_mem_decode = o_id_ex_decode;
+
+
+
+   // Memory阶段
+   assign o_dmem_addr = o_ex_mem_alu_result;
+   assign o_dmem_we = is_store; // 确保在存储操作时才启用DMEM写使能
+   assign o_dmem_towrite = o_ex_mem_dmem_towrite;
+   assign i_mem_wb_insn = o_ex_mem_insn;
+   assign i_mem_wb_alu_result = o_ex_mem_alu_result;
+   assign i_mem_wb_dmem_data = i_cur_dmem_data;
+   assign i_mem_wb_decode = o_ex_mem_decode;
+   assign i_mem_wb_sel = o_id_ex_sel;
+ 
+   assign mem_wb_we = 1'b1;
+
+   // Writeback阶段
+   assign i_wdata = (o_mem_wb_decode[5]) ? i_mem_wb_dmem_data : o_mem_wb_alu_result;
+   assign wsel = o_mem_wb_sel[8:6];
+
+   // 将信号通过寄存器传递到下一个流水线阶段
+   assign o_cur_pc = pc;
 
    /* Add $display(...) calls in the always block below to
     * print out debug information at the end of every cycle.
